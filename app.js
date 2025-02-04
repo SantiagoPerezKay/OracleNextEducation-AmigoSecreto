ccaed2d678d41243baa21e84b89fae0ea19a8fe8
Santiago Perez Kay <santiagoperezkay88@gmail.com>
2025-02-04 10:58:26 -0300

    amigo secreto terminado v1

--- app.js
+++ app.js
@@ -1,1 +1,34 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+const listaNombres = [];
+
+function agregarAmigo() {
+  let nombre = document.getElementById("amigo").value.trim();
+  //el trim() se lo agrego para limpiar espacios al principio y al final de la cadena.
+
+  nombre
+    ? (listaNombres.push(nombre), (document.getElementById("amigo").value = ""))
+    : alert("nombre nullo, ingrese un nombre valido");
+
+  agregarAmigosUl();
+}
+
+function agregarAmigosUl() {
+  let lista = document.getElementById("listaAmigos");
+  lista.innerHTML = "";
+
+  for (let index = 0; index < listaNombres.length; index++) {
+    let li = document.createElement("li");
+    li.textContent = listaNombres[index];
+    lista.appendChild(li);
+  }
+}
+
+function sortearAmigo() {
+  let numero = Math.floor(Math.random() * listaNombres.length);
+  let nombreSorteado = listaNombres[numero];
+
+  let sorteado = document.getElementById("resultado");
+  let li = document.createElement("li");
+  li.textContent = nombreSorteado;
+  sorteado.appendChild(li);
+}
